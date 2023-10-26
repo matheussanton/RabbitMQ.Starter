083966ac4e65f3edb50bcc914ba8eacfc12ebeba
matheussanton <matheussantong@gmail.com>
2023-10-26 19:50:15 -0300

    init

---
+++ Consumer/Consume.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:13377/") };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "starter.queue",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine($" [x] Received: {message}");
+            };
+            channel.BasicConsume(queue: "starter.queue",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Publisher/Publish.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:13377/"),
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "starter.queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+            bool running = true;
+            int option = 0;
+            while (running)
+            {
+                Console.WriteLine(@"
+                Choose an option:
+                1 - Publish a message
+                9 - Exit");
+
+                if (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid option\n");
+                    continue;
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter a message to publish:");
+                        var message = Console.ReadLine();
+                        BasicPublish(channel, message);
+                        break;
+                    case 9:
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+
+            }
+        }
+
+        private static void BasicPublish(IModel channel, string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "starter.queue",
+                                 basicProperties: null,
+                                 body: body);
+
+            Console.WriteLine($" [x] Sent {message}");
+        }
+    }
+}
